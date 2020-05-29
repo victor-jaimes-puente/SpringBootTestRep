e3b66c2e276f778e7aafd4611f01439533977cf9
VictorJP <victor.jaimes.puente@gmail.com>
2020-05-29 15:24:16 -0500

    spring practice

--- controllers/TestController.java
+++ controllers/TestController.java
@@ -1,4 +1,16 @@
 package com.codeup.springblogapp.controllers;
+import com.codeup.springblogapp.model.Book;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+    @Controller
 public class TestController {
-}
+
+        @GetMapping("/test")
+        @ResponseBody
+        public void hello() {
+        String name = "Victor";
+            System.out.println("Hello From Spring");        }
+    }
